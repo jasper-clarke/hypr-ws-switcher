6a14537af6b7051cfa203688fdf548da9ef287f0
Jasper <154771146+jasper-at-windswept@users.noreply.github.com>
2024-04-21 09:46:53 +1000

    Update ws-switch.sh

--- ws-switch.sh
+++ ws-switch.sh
@@ -1,6 +1,13 @@
-first=1
+# Set the number of your first/main monitor, find it under `hyrpctl monitors` then find your monitor and it is the ID: value at the top.
+first=1 
+# Set the number of your secondary monitor
 second=0
+# Set the number of your third monitor
+# third=2
+
+# Set the number of workspaces per screen
 ws_per_monitor=3
+
 
 monitor=$(hyprctl activeworkspace | grep -E "monitorID: " | sed -e 's/^[ \t]*//')
 if [[ $monitor == *"$first"* ]]; then
@@ -16,4 +23,11 @@ elif [[ $monitor == *"$second"* ]]; then
     else
         hyprctl dispatch workspace $calc
     fi
+# elif [[ $monitor == *"$third"* ]]; then
+#     calc=$(calc "$1 + $ws_per_monitor" | sed -e 's/^[ \t]*//' )
+#     if [[ $2 == "move" ]]; then
+#         hyprctl dispatch movetoworkspace $calc
+#     else
+#         hyprctl dispatch workspace $calc
+#     fi
 fi
